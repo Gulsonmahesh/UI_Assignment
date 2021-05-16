89916cdf1f24d23ef1476a8d3d0458c62597af64
Gulson Mahesh <63383483+Gulsonmahesh@users.noreply.github.com>
2021-05-16 14:04:52 +0530

    Added Caroual
    Added Caroual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {IvyCarouselModule} from 'angular-responsive-carousel';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { CarousalComponent } from './components/carousal/carousal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CarousalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IvyCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
